7069c9c9a18cf097e978291015072f6e9b595e0d
Patricia Fiona <patriciafiona3@gmail.com>
2023-12-16 07:04:38 +0700

    [Clean Architecture] Finished study case part two

--- Codelabs/TourismProject/app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ Codelabs/TourismProject/app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -7,6 +7,7 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiConfig
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.JsonHelper
 
@@ -14,7 +15,7 @@ object Injection {
     fun provideRepository(context: Context): TourismRepository {
         val database = TourismDatabase.getInstance(context)
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
         val localDataSource = LocalDataSource.getInstance(database.tourismDao())
         val appExecutors = AppExecutors()
 
